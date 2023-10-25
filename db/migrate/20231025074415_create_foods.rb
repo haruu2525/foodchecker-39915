8eb014a5728086e10f42b22d84f4056a4393be9c
haruu2525 <tech.haru551@gmail.com>
2023-10-25 16:53:31 +0900

    foodsテーブルのカラム追加

--- db/migrate/20231025074415_create_foods.rb
+++ db/migrate/20231025074415_create_foods.rb
@@ -1,7 +1,9 @@
 class CreateFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :foods do |t|
-
+      t.string :name
+      t.string :expiration_dete
+      t.string :remaeks
       t.timestamps
     end
   end
